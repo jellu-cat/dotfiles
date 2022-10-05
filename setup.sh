1d43e6665708b3d4178039dbee5aff30a3941105
Jellu <105118442+Debilesque@users.noreply.github.com>
2022-10-04 22:19:07 -0500

    setup script created

--- setup.sh
+++ setup.sh
@@ -1,11 +1,10 @@
 CONFIG="$HOME/.config"
 PWD="$(pwd)"
 
+echo "$PWD"
+echo "$CONFIG"
+
 mkdir -v $CONFIG
-
-## mkdir -vp $CONFIG/{bspwm,feh,git,i3,kitty,mpv,nvim,redshift,rofi,stalonetray,starship,sxhkd,gtk-3.0}
-
-pwd
 
 ln -s $PWD/bspwm $CONFIG
 ln -s $PWD/feh $CONFIG
@@ -19,3 +18,8 @@ ln -s $PWD/stalonetray $CONFIG
 ln -s $PWD/starship $CONFIG
 ln -s $PWD/sxhkd $CONFIG
 ln -s $PWD/gtk-3.0 $CONFIG
+
+ln -s $PWD/bash/.bash* $HOME
+sudo ln -s $PWD/libinput/* /etc/X11/xorg.conf.d/
+ln -s $PWD/Xmodmap/.* $HOME
+ln -s $PWD/xinit/.* $HOME
