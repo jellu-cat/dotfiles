7be6c3b91842d2e460b7a3ad5dd5aba4d2ddd1b4
Jellu <105118442+Debilesque@users.noreply.github.com>
2023-11-18 14:52:26 -0600

    creationg mechanism of folders improved

--- setup.sh
+++ setup.sh
@@ -1,26 +1,21 @@
 CONFIG="$HOME/.config"
 PWD="$(pwd)"
 
-echo "$PWD"
-echo "$CONFIG"
-
 mkdir -v $CONFIG
 
-ln -s $PWD/bspwm $CONFIG
+for i in bspwm git i3 kitty mpv redshift rofi \
+    sxhkd gtk-3.0 lf xplr polybar qutebrowser \
+    ranger sioyek nyxt openbox; do
+    if [ -d "$i" ]; then
+        ln -s $PWD/$i $CONFIG
+    fi
+done
+
+## since sioyek was installed via flatpak, its configuration
+# file need to be here
+ln -s $PWD/sioyek $HOME/.var/app/com.github.ahrm.sioyek/config/sioyek/
+
 ln -s $PWD/feh/.fehbg $HOME
-ln -s $PWD/git $CONFIG
-ln -s $PWD/i3 $CONFIG
-ln -s $PWD/kitty $CONFIG
-ln -s $PWD/mpv $CONFIG
-ln -s $PWD/redshift $CONFIG
-ln -s $PWD/rofi $CONFIG
-ln -s $PWD/stalonetray $CONFIG
-ln -s $PWD/starship $CONFIG
-ln -s $PWD/sxhkd $CONFIG
-ln -s $PWD/gtk-3.0 $CONFIG
-ln -s $PWD/lf $CONFIG
-ln -s $PWD/xplr $CONFIG
-ln -s $PWD/polybar/ $CONFIG
 
 ln -s $PWD/bash/.bash* $HOME
 ln -s $PWD/Xmodmap/.* $HOME
@@ -35,4 +30,3 @@ sudo ln -s $PWD/libinput/* /etc/X11/xorg.conf.d/
 mkdir ~/{desktop,downloads,templates,public,documents,music,pictures,videos}
 
 xdg-user-dirs-update
-
